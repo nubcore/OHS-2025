f831628ac83beb8d6770f01821d2ff3dad4ecd42
NUBcore <beau@nubcore.com>
2025-06-03 22:57:37 +0200

    SPARKLEz@Ungdomshuset

--- examples/matrix/main.py
+++ examples/matrix/main.py
@@ -6,6 +6,7 @@
 from machine import Pin
 from time import sleep_ms
 import neopixel
+import random
 
 # todo: change to const
 PIXELS_PIN = 20
@@ -13,7 +14,6 @@ MATRIX_PIN = 5
 PIXELS_COUNT = 2
 MATRIX_COUNT = 16
 BLINK_DELAY = 333
-
 
 np = neopixel.NeoPixel(Pin(PIXELS_PIN), PIXELS_COUNT)
 grid = neopixel.NeoPixel(Pin(MATRIX_PIN), MATRIX_COUNT)
@@ -30,6 +30,30 @@ def cycle():
   pos = 0
  sleep_ms(100)
 
+def rgb_test():
+ grid.fill((5, 0, 0))
+ grid.write()
+ sleep_ms(500)
+ grid.fill((0, 5, 0))
+ grid.write()
+ sleep_ms(500)
+ grid.fill((0, 0, 5))
+ grid.write()
+ sleep_ms(300)
+
+def r():
+ return max(random.randint(0,32)-20, 0)
+
+def r3():
+ return (r(),r(),r())
+
+def sparkle():
+ grid[random.randint(0,MATRIX_COUNT-1)] = r3()
+ grid.write()
+
+while 1:
+ sparkle()
+
 while 1:
  np[0] = (0, 20, 0)
  np[1] = (0, 0, 20)
@@ -40,15 +64,3 @@ while 1:
  np.write()
  sleep_ms(100)
 
- while 1:
-  cycle()
-
- grid.fill((5, 0, 0))
- grid.write()
- sleep_ms(500)
- grid.fill((0, 5, 0))
- grid.write()
- sleep_ms(500)
- grid.fill((0, 0, 5))
- grid.write()
- sleep_ms(300)
