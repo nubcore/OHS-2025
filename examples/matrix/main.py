9d9eeecd43eebfdf6c175428c4b0dcc05e81ed4d
NUBcore <beau@nubcore.com>
2025-06-03 23:37:26 +0200

    more sparkle and init dance

--- examples/matrix/main.py
+++ examples/matrix/main.py
@@ -1,7 +1,7 @@
 # Blinky for OHS2025 Badge
 # Toggles Blue/Green LEDs
 # Created by beau@nubcore.dk
-# Revised: 31.05.2025 12:34
+# Revised: 06.03.2025 23:29
 
 from machine import Pin
 from time import sleep_ms
@@ -13,14 +13,17 @@ PIXELS_PIN = 20
 MATRIX_PIN = 5
 PIXELS_COUNT = 2
 MATRIX_COUNT = 16
-BLINK_DELAY = 333
 
 np = neopixel.NeoPixel(Pin(PIXELS_PIN), PIXELS_COUNT)
 grid = neopixel.NeoPixel(Pin(MATRIX_PIN), MATRIX_COUNT)
 
+np.fill((0,0,0))
+grid.fill((0,0,0))
+sleep_ms(100)
+
 pos = 0
 
-def cycle():
+def cycle(delay=20):
  global pos
  grid.fill((0,0,0))
  grid[pos] = (20, 0, 0)
@@ -28,18 +31,20 @@ def cycle():
  pos = pos + 1
  if pos >= MATRIX_COUNT:
   pos = 0
- sleep_ms(100)
+ sleep_ms(delay)
 
-def rgb_test():
- grid.fill((5, 0, 0))
- grid.write()
- sleep_ms(500)
- grid.fill((0, 5, 0))
- grid.write()
- sleep_ms(500)
- grid.fill((0, 0, 5))
- grid.write()
- sleep_ms(300)
+def rgb_test(pixels, delay=100):
+ pixels.fill((0, 0, 0))
+ pixels.write()
+ sleep_ms(delay)
+ pixels.fill((5, 0, 0))
+ pixels.write()
+ sleep_ms(delay)
+ pixels.fill((0, 5, 0))
+ pixels.write()
+ sleep_ms(delay)
+ pixels.fill((0, 0, 5))
+ pixels.write()
 
 def r():
  return max(random.randint(0,32)-20, 0)
@@ -50,17 +55,19 @@ def r3():
 def sparkle():
  grid[random.randint(0,MATRIX_COUNT-1)] = r3()
  grid.write()
+ np[random.randint(0,PIXELS_COUNT-1)] = r3()
+ np.write()
 
+# Initialization dance
+for x in range(32):
+ cycle()
+
+for y in range(3):
+ rgb_test(np, 20)
+
+for x in range(3):
+ rgb_test(grid)
+
+# GO GO SPARKLEz
 while 1:
  sparkle()
-
-while 1:
- np[0] = (0, 20, 0)
- np[1] = (0, 0, 20)
- np.write()
- sleep_ms(100)
- np[0] = (0, 0, 20)
- np[1] = (0, 20, 0)
- np.write()
- sleep_ms(100)
-
---
+++ examples/matrix/mpe.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Utility script to edit local, copy to remote and run
+nano main.py; mpremote fs cp main.py :.; mpremote
